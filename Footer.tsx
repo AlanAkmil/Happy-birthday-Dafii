bcaba1e90e0ff857322004c57453dd468e863536
AlanAkmil <alansubagyo058@gmail.com>
2026-08-02 19:42:51 +0700

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="relative z-10 px-6 pb-14 pt-6 text-center">
-      <p className="tag text-[11px] text-smoke">
-        server dijaga oleh Alan &middot; {new Date().getFullYear()}
-      </p>
-    </footer>
-  );
-}
